dbe12ea1297fa61ad9ca79569cd52d100de12b2e
Jonathan Yee <Jonathan.Yee.W@gmail.com>
2018-08-11 15:21:40 -0700

    updated the user name and likes button (users can only like a post once per visit)

--- react-client/src/view/Components/Post.js
+++ react-client/src/view/Components/Post.js
@@ -8,7 +8,13 @@ var postManager = require('../../controller/postmanager')
 // -item | the Post object
 
 export class Post extends Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            liked: false
+        }
+    }
+
     likePost = () => {
         console.log("## Post ## likePost()")
         postManager.likePost(this.props.item)
@@ -24,6 +30,35 @@ export class Post extends Component {
     }
 
     render() {
+        // Code below is to ensure that people have to revisit the page
+        // to like the button each time. Can't hit the like button mulitple times
+        // only once per visit
+        let likeButton;
+        if (this.state.liked === false) {
+            likeButton =
+                <a href="#" onClick={(event) => {
+                    event.preventDefault()
+                    this.setState({ liked: true })
+                    this.likePost()
+                }} >Likes {this.props.item.likes}</a>
+        } else {
+            likeButton =
+                <a href="#" onClick={(event) => {
+                    event.preventDefault()
+                }} >Likes {this.props.item.likes + 1}</a>
+        }
+
+        // User name displayed in the event non is submitted
+        let userName;
+        if(this.props.item.name === ""){
+            userName = 
+            <p className="text-muted">anonymous</p>
+        } else{
+            userName = 
+            <p className="text-muted">{this.props.item.name}</p>
+        }
+
+
         return (
             <div className="mt-3 mb-3">
                 <div className="border rounded grateful-fill-width grateful-post-box">
@@ -33,14 +68,16 @@ export class Post extends Component {
                 </div>
                 <div className="row">
                     <div className="col-4">
-                        <p>{this.props.item.name}</p>
+                        {/* <p>{this.props.item.name}</p> */}
+                        {userName}
                     </div>
                     <div className="col-2">
                         {/* <button className="btn btn-sm btn-outline-primary">post.likes</button> */}
-                        <a href="#" onClick={(event) => {
+                        {/* <a href="#" onClick={(event) => {
                             event.preventDefault()
                             this.likePost()
-                        }} >Like {this.props.item.likes}</a>
+                        }} >Like {this.props.item.likes}</a> */}
+                        {likeButton}
                     </div>
                     <div className="col-4">
                         {/* <button className="btn btn-sm btn-outline-primary">post.likes</button> */}
